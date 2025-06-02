0d8a725efaeeab619130bfe8659cbad573b6b4f4
Lczin699 <banpecadodaraposa69@gmail.com>
2025-06-02 20:13:45 -0300

    Add files via upload

---
+++ AULA 15-02.06.25/script.js
@@ -0,0 +1,17 @@
+// Declarando
+let x
+let y
+let z
+
+//Atribuindo
+x = 10
+y = 12
+z = 14
+
+if (x > y && x > z) {
+    console.log("x é o maior")
+} else if (y > x && y > z) {
+    console.log("y é o maior")
+} else {
+    console.log("z é o maior")
+}
