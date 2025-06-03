cbe992459cff9a10a93a1cc91594806711d79737
Lczin699 <banpecadodaraposa69@gmail.com>
2025-06-03 00:27:05 +0000

    exercicios

--- AULA 15-02.06.25/script.js
+++ AULA 15-02.06.25/script.js
@@ -1,17 +1,21 @@
-// Declarando
-let x
-let y
-let z
-
-//Atribuindo
-x = 10
-y = 12
-z = 14
-
-if (x > y && x > z) {
-    console.log("x é o maior")
-} else if (y > x && y > z) {
-    console.log("y é o maior")
-} else {
-    console.log("z é o maior")
-}
+// Exercicio 01 - Crie um programa que some dois números e exiba o resultado no console.
+// Exercicio 02 - Crie um programa que subtraia dois números e exiba o resultado no console.
+// Exercicio 03 - Crie um programa que multiplique dois números e exiba o resultado no console.
+// Exercicio 04 - Crie um programa que divida dois números e exiba o resultado no console.
+// Exercicio 05 - Crie um programa que calcule o resto da divisão de dois números e exiba o resultado no console.
+// Exercicio 06 - Crie um programa que calcule a média de dois números e exiba o resultado no console.
+// Exercicio 07 - Crie um programa que verifique se um número é par e exiba o resultado no console.
+// Exercicio 08 - Crie um programa que verifique se um número é positivo, negativo ou zero e exiba o resultado no console.
+// Exercicio 09 - Crie um programa que verifique se um número é maior, menor ou igual a outro número e exiba o resultado no console.
+// Exercicio 10 - Crie um programa que verifique se um número é múltiplo de outro número e exiba o resultado no console.
+// Exercicio 11 - Crie um programa que verifique se uma string contém uma determinada substring e exiba o resultado no console.
+// Exercicio 12 - Crie um um programa que receba o raio de uma circunferência e mostre o diâmetro, comprimento e área desta.
+// Exercicio 13 - Crie um programa que receba três inteiros e diga qual deles é o maior e qual o menor. Consegue criar mais de uma solução?
+// Exercicio 14 - Crie um programa que converta uma string para número e exiba o resultado no console.
+let numbernumero = "50"
+let numero = parseInt(numbernumero)
+console.log(numero + 50)
+// Exercicio 15 - Crie um programa que recebe uma nota e checa se você passou direto, ficou de recuperação ou foi reprovado na matéria.
+// Exercicio 15 - Crie um programa que converta um número para string e exiba o resultado no console.
+// Exercicio 16 - Crie um programa que verifique se uma string é um palíndromo e exiba o resultado no console..
+// Exercicio 17 - Crie um programa que verifique se uma string é um anagrama de outra string e exiba o resultado no console.
