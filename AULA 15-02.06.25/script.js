7019e0cb22e3653371eb42f38d1c7a03f021809b
Lczin699 <banpecadodaraposa69@gmail.com>
2025-06-03 13:02:01 +0000

    Exercícios

--- AULA 15-02.06.25/script.js
+++ AULA 15-02.06.25/script.js
@@ -1,40 +1,63 @@
 // Exercicio 01 - Crie um programa que some dois números e exiba o resultado no console.
+console.log()
+console.log("Exercicio 01 - Crie um programa que some dois números e exiba o resultado no console.")
+console.log()
 
 let x = 10
 let y = 40
 let soma = x + y
 console.log("Resultado da soma: " + soma)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 02 - Crie um programa que subtraia dois números e exiba o resultado no console.
+
+console.log("Exercicio 02 - Crie um programa que subtraia dois números e exiba o resultado no console.")
+console.log()
 
 x = 40
 y = 10
 let subtracao = x - y
 console.log("Resultado da subtração: " + subtracao)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 03 - Crie um programa que multiplique dois números e exiba o resultado no console.
+
+console.log("Exercicio 03 - Crie um programa que multiplique dois números e exiba o resultado no console.")
+console.log()
 
 x = 40
 y = 10
 let multiplicacao = x * y
 console.log("Resultado da multiplicação: " + multiplicacao)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 04 - Crie um programa que divida dois números e exiba o resultado no console.
+
+console.log("Exercicio 04 - Crie um programa que divida dois números e exiba o resultado no console.")
+console.log()
 
 x = 40
 y = 10
 let divisao = x / y
 console.log("Resultado da divisão: " + divisao)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 05 - Crie um programa que calcule o resto da divisão de dois números e exiba o resultado no console.
+
+console.log("Exercicio 05 - Crie um programa que calcule o resto da divisão de dois números e exiba o resultado no console.")
+console.log()
 
 x = 30
 y = 20
@@ -46,9 +69,15 @@ if (resto === 0){
     console.log("O resto da divisão de: " + x + " por " + y + " é: " + resto)
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 06 - Crie um programa que calcule a média de dois números e exiba o resultado no console.
+
+console.log("Exercicio 06 - Crie um programa que calcule a média de dois números e exiba o resultado no console.")
+console.log()
+
 
 x = 10
 y = 5
@@ -56,9 +85,15 @@ soma = x + y
 let media = soma / 2
 console.log("A média é: " + media)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 07 - Crie um programa que verifique se um número é par e exiba o resultado no console.
+
+console.log("Exercicio 07 - Crie um programa que verifique se um número é par e exiba o resultado no console.")
+console.log()
+
 
 x = 2
 
@@ -68,9 +103,15 @@ if (x % 2 === 0) {
     console.log("O número é IMPAR!")
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 08 - Crie um programa que verifique se um número é positivo, negativo ou zero e exiba o resultado no console.
+
+console.log("Exercicio 08 - Crie um programa que verifique se um número é positivo, negativo ou zero e exiba o resultado no console.")
+console.log()
+
 
 x = 10
 
@@ -82,9 +123,15 @@ if (x > 0){
     console.log("O número é ZERO!")
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 09 - Crie um programa que verifique se um número é maior, menor ou igual a outro número e exiba o resultado no console.
+
+console.log("Exercicio 09 - Crie um programa que verifique se um número é maior, menor ou igual a outro número e exiba o resultado no console.")
+console.log()
+
 
 x = 10
 y = 10
@@ -97,9 +144,15 @@ if (x > y) {
     console.log(x + " É igual a " + y)
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 10 - Crie um programa que verifique se um número é múltiplo de outro número e exiba o resultado no console.
+
+console.log("Exercicio 10 - Crie um programa que verifique se um número é múltiplo de outro número e exiba o resultado no console.")
+console.log()
+
 
 x = 20
 y = 0
@@ -110,8 +163,15 @@ if ( x % y == 0 ) {
     console.log("O número " + x + " não é multiplo de " + y)
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
+
 // Exercicio 11 - Crie um programa que verifique se uma string contém uma determinada substring e exiba o resultado no console.
+
+console.log("Exercicio 11 - Crie um programa que verifique se uma string contém uma determinada substring e exiba o resultado no console.")
+console.log()
+
 
 let textocompleto = "Fala rapaziada, tudo tranquilo?"
 let pedacodotexto = "Fala"
@@ -122,10 +182,15 @@ if (textocompleto.includes(pedacodotexto)){
     console.log(textocompleto + " NÃO CONTÉM " + pedacodotexto)
 }
 
+console.log()
 console.log("-----------------------------------------------")
 console.log()
 
 // Exercicio 12 - Crie um um programa que receba o raio de uma circunferência e mostre o diâmetro, comprimento e área desta.
+
+console.log("Exercicio 12 - Crie um um programa que receba o raio de uma circunferência e mostre o diâmetro, comprimento e área desta.")
+console.log()
+
 
 let raio = 5
 let diametro = 2 * raio
@@ -137,9 +202,15 @@ console.log()
 console.log("Diâmetro: " + diametro.toFixed(2))
 console.log("Comprimento: " + comprimentro.toFixed(2))
 console.log("Área: " + area.toFixed(2))
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 13 - Crie um programa que receba três inteiros e diga qual deles é o maior e qual o menor. Consegue criar mais de uma solução?
+
+console.log("Exercicio 13 - Crie um programa que receba três inteiros e diga qual deles é o maior e qual o menor. Consegue criar mais de uma solução?")
+console.log()
+
 
 x = 20
 y = 30
@@ -153,15 +224,23 @@ if (x > y && X > z){
     console.log("Z é o maior")
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 14 - Crie um programa que converta uma string para número e exiba o resultado no console.
+
+console.log("Exercicio 14 - Crie um programa que converta uma string para número e exiba o resultado no console.")
+console.log()
+
 
 let numbernumero = "50"
 let numero = parseInt(numbernumero)
 console.log(numero + 50)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 15 - Crie um programa que recebe uma nota e checa se você passou direto, ficou de recuperação ou foi reprovado na matéria.
 
@@ -175,17 +254,29 @@ if (nota >= 7){
     console.log("REPROVADO!")
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 16 - Crie um programa que converta um número para string e exiba o resultado no console.
+
+console.log("Exercicio 16 - Crie um programa que converta um número para string e exiba o resultado no console.")
+console.log()
+
 
 numero = 30
 numbernumero = numero.toString()
 console.log(numbernumero)
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 17 - Crie um programa que verifique se uma string é um palíndromo e exiba o resultado no console.
+
+console.log("Exercicio 17 - Crie um programa que verifique se uma string é um palíndromo e exiba o resultado no console.")
+console.log()
+
 
 let textorigin = "Arara"
 let textolimpo = textorigin.toLowerCase().replace(/[^a-z0-9]/g, '')
@@ -197,12 +288,18 @@ if (textolimpo === textoinvertido) {
     console.log(textorigin + " Não é um PALÍNDROMO.");
 }
 
+console.log()
 console.log("-----------------------------------------------")
+console.log()
 
 // Exercicio 18 - Crie um programa que verifique se uma string é um anagrama de outra string e exiba o resultado no console.
 
+console.log("Exercicio 18 - Crie um programa que verifique se uma string é um anagrama de outra string e exiba o resultado no console.")
+console.log()
+
+
 let p1 = "lordw"
-let p2 = "wo"
+let p2 = "world"
 
 let p1minuscula = p1.toLowerCase()
 let p1limpa = p1minuscula.replace(/[^a-z0-9]/g, '') 
@@ -221,3 +318,5 @@ if (p1final === p2final) {
 }else {
     console.log(p1 + " e " + p2 + " não são anagramas")
 }
+
+console.log()
