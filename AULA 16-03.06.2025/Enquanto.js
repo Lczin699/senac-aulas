9801183e171d0b715f67578d3f6e99aa4581b68a
Lczin699 <banpecadodaraposa69@gmail.com>
2025-06-04 14:20:29 +0000

    Exercicios

--- AULA 16-03.06.2025/Enquanto.js
+++ AULA 16-03.06.2025/Enquanto.js
@@ -6,7 +6,7 @@
 
         let x = 1
         let soma = 0
-        let n = 10
+        let n = 5
         
 
         while (x <= n) {
@@ -16,14 +16,27 @@
 
         }
         console.log()
-        console.log("--------------------------------------")
+        console.log("--------------------------------------");
 
 
 // Ler um número inteiro n. Escrever a soma de todos os números pares de 2 até n.
+
 console.log()
 console.log("Exercicio 02 - Ler um número inteiro n. Escrever a soma de todos os números pares de 2 até n.")
 console.log()
 
+
+        x = 2
+        soma = 0
+        n = 20
+
+        while (soma + x <= n) {
+        soma += x
+        console.log(soma)
+        x += 2
+
+        } console.log(soma)
+        
 
 
 
