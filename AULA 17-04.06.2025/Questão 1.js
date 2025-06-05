a02a3a6b3a17141ac117343bec3b1cf97bd1b367
Lczin699 <banpecadodaraposa69@gmail.com>
2025-06-05 12:12:25 +0000

    Exercicios

--- AULA 17-04.06.2025/Questão 1.js
+++ AULA 17-04.06.2025/Questão 1.js
@@ -1,11 +1,26 @@
-let n = 5 
+let n = 5
 
-if (n > 0) {
-console.log('Numero positivo')
-} else {
-    console.log('Numero n é positivo')
-}
 
 while (n > 0) {
-    
+    if (n > 0) {
+        console.log(n + ' patinhos foram passear')
+        console.log('Além das montanhas')
+        console.log('Para brincar')
+        n = n - 1
+        console.log('A mamãe gritou: Quá, quá, quá, quá, mas só ' + n + ' patinhos voltaram de lá.')
+        console.log()
+    } else {
+
+    }
+
 }
+console.log()
+console.log('A mamãe patinha foi procurar')
+console.log('Além das montanhas')
+console.log('Na beira do mar')
+console.log('A mamãe gritou: Quá, quá, quá, quá')
+n = 5
+console.log('E os ' + n + ' patinhos voltaram de lá.')
+
+
+
